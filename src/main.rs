55d06fe58496d86965c60dbb35726a40fa174ef3
Pham Nhat Minh <p.nhatminh1502@gmail.com>
2023-05-14 15:27:33 +0700

    add termion to handle input from CLI

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use std::io::{self, Read};
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
 
 fn main() {
+    let _stdout = stdout().into_raw_mode().unwrap();
+
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control() {
+            println!("{:?} \r", b);
+        } else {
+            println!("{:?} ({})\r", b, c);
+        }
         if c == 'q' {
             println!("Program is exiting...");
             break;
         }
-        println!("{}", c);
     }
 }
