99364475fd2f1239c18ff1a7eb75ab4090c92b56
Pham Nhat Minh <p.nhatminh1502@gmail.com>
2023-05-14 16:29:45 +0700

    cursor control and display

--- src/editor.rs
+++ src/editor.rs
@@ -3,9 +3,15 @@ use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
+    cursor_position: Position
 }
 
 impl Editor {
@@ -27,18 +33,19 @@ impl Editor {
     pub fn default() -> Self {
         Self{
             should_quit: false,
-            terminal: Terminal::default().expect("Failed to initialize terminal")
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+            cursor_position: Position { x: 0, y: 0 }
         }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::clear_screen();
-        Terminal::cursor_position(0, 0);
+        Terminal::cursor_position(&Position { x: 0, y: 0 });
         if self.should_quit {
             println!("Goodbye.\r");
         } else {
             self.draw_rows();
-            Terminal::cursor_position(0, 0);
+            Terminal::cursor_position(&self.cursor_position);
         }
         Terminal::flush()
     }
@@ -47,16 +54,44 @@ impl Editor {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('x') => self.should_quit = true,
-            Key::Char(c) => {
-                if c.is_control() {
-                    println!("{:?}\r", c as u8);
-                } else {
-                    println!("{:?} ({})\r", c as u8, c);
-                }
-            }
+            Key::Up 
+            | Key::Down 
+            | Key::Left 
+            | Key::Right 
+            | Key::PageUp
+            | Key::PageDown
+            | Key::End
+            | Key::Home => self.move_cursor(pressed_key),
             _ => (),
         }
         Ok(())
+    }
+
+    fn move_cursor(&mut self, key: Key) {
+        let Position{mut y, mut x} = self.cursor_position;
+        let size = self.terminal.size();
+        let height = size.height.saturating_sub(1) as usize;
+        let width = size.width.saturating_sub(1) as usize;
+        match key {
+            Key::Up => y = y.saturating_sub(1),
+            Key::Down => {
+                if y < height {
+                    y = y.saturating_add(1)
+                }
+            }
+            Key::Left => x = x.saturating_sub(1),
+            Key::Right => {
+                if x < width {
+                    x = x.saturating_add(1);
+                }
+            }
+            Key::PageUp => y = 0,
+            Key::PageDown => y = height,
+            Key::Home => x = 0,
+            Key::End => x = width,
+            _ => ()
+        }
+        self.cursor_position = Position { x, y }
     }
 
     fn draw_rows(&self) {
