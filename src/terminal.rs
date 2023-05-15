3594ad682c6e9caba58fc8911610393d7599b6e4
Pham Nhat Minh <p.nhatminh1502@gmail.com>
2023-05-15 18:26:16 +0700

    add status bar

--- src/terminal.rs
+++ src/terminal.rs
@@ -2,6 +2,7 @@ use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
+use termion::color;
 
 use crate::editor::Position;
 
@@ -21,7 +22,7 @@ impl Terminal {
         Ok(Self {
             size: Size {
                 width: size.0,
-                height: size.1
+                height: size.1.saturating_add(2),
             },
             _stdout: stdout().into_raw_mode()?
         })
@@ -63,5 +64,17 @@ impl Terminal {
     }
     pub fn clear_current_line() {
         print!("{}", termion::clear::CurrentLine);
+    }
+    pub fn set_bg_color(color: color::Rgb) {
+        print!("{}", color::Bg(color));
+    }
+    pub fn reset_bg_color() {
+        print!("{}", color::Bg(color::Reset));
+    }
+    pub fn set_fg_color(color: color::Rgb) {
+        print!("{}", color::Fg(color));
+    }
+    pub fn reset_fg_color() {
+        print!("{}", color::Fg(color::Reset));
     }
 }
