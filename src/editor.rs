f8788e04ad601af0067dce5323af2ee6f26cc4e4
Pham Nhat Minh <p.nhatminh1502@gmail.com>
2023-05-15 15:49:34 +0700

    add document and row

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,11 @@
+use crate::Document;
 use crate::Terminal;
+use crate::Row;
 use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[derive(Default)]
 pub struct Position {
     pub x: usize,
     pub y: usize,
@@ -11,7 +14,8 @@ pub struct Position {
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
-    cursor_position: Position
+    cursor_position: Position,
+    document: Document,
 }
 
 impl Editor {
@@ -34,13 +38,14 @@ impl Editor {
         Self{
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
-            cursor_position: Position { x: 0, y: 0 }
+            cursor_position: Position::default(),
+            document: Document::open(),
         }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::clear_screen();
-        Terminal::cursor_position(&Position { x: 0, y: 0 });
+        Terminal::cursor_position(&Position::default());
         if self.should_quit {
             println!("Goodbye.\r");
         } else {
@@ -94,12 +99,21 @@ impl Editor {
         self.cursor_position = Position { x, y }
     }
 
+    pub fn draw_row(&self, row: &Row) {
+        let start = 0;
+        let end = self.terminal.size().width as usize;
+        let row = row.render(start, end);
+        println!("{}\r", row)
+    }
+
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
-        for row in 0..height - 1 {
+        for terminal_row in 0..height - 1 {
             Terminal::clear_current_line();
-            if row == height / 3 {
-                self.draw_welcome_message();
+            if let Some(row) = self.document.row(terminal_row as usize) {
+                self.draw_row(row);
+            } else if terminal_row == height / 3 {
+                self.draw_welcome_message()
             } else {
                 println!("~\r");
             }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![warn(clippy::all, clippy::pedantic)]            
 mod editor;
 mod terminal;
+mod document;
+mod row;
+pub use document::Document;
+pub use row::Row;
 use editor::Editor;
 pub use terminal::Terminal;
 fn main() {
