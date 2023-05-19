7bb27880bb29aa8d13dafc62448ec78a62f5479c
Pham Nhat Minh <p.nhatminh1502@gmail.com>
2023-05-19 23:50:18 +0700

    colorful search results

--- src/editor.rs
+++ src/editor.rs
@@ -153,6 +153,7 @@ impl Editor {
                     } else if moved {
                         editor.move_cursor(Key::Left);
                     }
+                    editor.document.highlight(Some(query));
                 },
             )
             .unwrap_or(None);
@@ -161,6 +162,7 @@ impl Editor {
             self.cursor_position = old_position;
             self.scroll();
         }
+        self.document.highlight(None);
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
